ae251dbfc0b1758ca6ab7d164f4c5063617dc0f8
nishantsingh1107 <nishantsingh0311@gmail.com>
2025-07-17 14:12:19 +0530

    jwthelper

--- utils/jwtHelpers.js
+++ utils/jwtHelpers.js
@@ -6,7 +6,7 @@ const attachJWTToken = (res, data) => {
     res.cookie("authorization", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         secure: true, // only sent over https connections
-        sameSite: "Strict", // only our backend will get this cookie (no other backend can access it)
+        sameSite: "None", // only our backend will get this cookie (no other backend can access it)
         httpOnly: true, // frontend will not be able to read this cookie
         // so that our token is out of reach of javascript --> hackers
     });
@@ -16,7 +16,7 @@ const removeJWTToken = (res) => {
     res.cookie("authorization", "", {
         maxAge: 0,
         secure: true, // only sent over https connections
-        sameSite: "Strict", // only our backend will get this cookie (no other backend can access it)
+        sameSite: "None", // only our backend will get this cookie (no other backend can access it)
         httpOnly: true, // frontend will not be able to read this cookie
         // so that our token is out of reach of javascript --> hackers
     });
